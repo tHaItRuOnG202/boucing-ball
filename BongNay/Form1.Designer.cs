b88e6f8168828960c7f6f4a125bcd6ace533d89e
tHaItRuOnG123 <91905960+tHaItRuOnG123@users.noreply.github.com>
2022-07-11 22:10:07 +0700

    jumping

---
+++ BongNay/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BongNay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.Image = global::BongNay.Properties.Resources.ball;
+            this.picBall.Location = new System.Drawing.Point(280, 118);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(81, 74);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(636, 388);
+            this.Controls.Add(this.picBall);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tâng bóng";
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BongNay/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BongNay
+{
+    public partial class Form1 : Form
+    {
+        int dx = 5; // Độ dời theo phương ngang
+        int dy = 7; // Độ dời theo phương dọc
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Nếu quả bóng đụng biên ngang --> đổi chiều
+            if (picBall.Left <= 0 || picBall.Right >= ClientRectangle.Width)
+                dx = -dx;
+            // Nếu quả bóng đụng biên trên và dưới --> đổi chiều
+            if (picBall.Top <= 0 || picBall.Bottom >= ClientRectangle.Height)
+                dy = -dy;
+            // Di chuyển quả bóng theo dx, dy
+            picBall.Left += dx;
+            picBall.Top += dy;
+        }
+    }
+}
